90d22d78ecb2080222b39d3fed7887bcaaf990c2
Pranav-57 <harshaher0404@gmail.com>
2021-10-19 22:11:31 +0530

    Update media file path

--- online_tiffin_service/settings.py
+++ online_tiffin_service/settings.py
@@ -112,8 +112,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "your_media_folder")
 
